5d5a31ccfff8048df0befda23a761c265f45c5cf
JustineBiancaMStaRomana <justinebianca.staromana@smc.pshs.edu.ph>
2025-01-27 10:21:53 +0800

    Changed formula for problem 1

--- q3fa1p1.js
+++ q3fa1p1.js
@@ -1,7 +1,13 @@
 var nickname = "Justine";
-var height = "a\'b\"";
-var weight = "idk.idk kg";
+var height = 62;
+var weight = 45;
+
+var height1 = parseInt(62/12);
+var height2 = 62%12;
+
+weight = parseInt(weight*2.2);
+weight = weight.toFixed(2);
 
 function prob1() {
-    alert("Name: " + nickname + " \nHeight: " + height + " \nWeight: " + weight);
+    alert("Name: " + nickname + " \nHeight: " + height1 + "\'" + height2 + "\"" + " \nWeight: " + weight + " lbs");
 }
