03b1487ec3327bd0013dbdb2e4e6a5e7e7f6c470
JustineBiancaMStaRomana <justinebianca.staromana@smc.pshs.edu.ph>
2025-01-27 10:28:55 +0800

    Udpated console output on problem 3

--- q3fa1p3.js
+++ q3fa1p3.js
@@ -1,6 +1,6 @@
 function prob3() {
     if(confirm("Do you consent to sharing personal information with index.html?")){
-        console.log("Name: " + nickname + " \nHeight: " + height + " \nWeight: " + weight);
+        console.log("Name: " + nickname + " \nHeight: " + height1 + "\'" + height2 + "\"" + " \nWeight: " + weight + " lbs");
     }else{
         console.log("User does not wish to share his/her information.");
     }
