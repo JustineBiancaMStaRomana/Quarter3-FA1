f39a879e8dcbe6f5e2a68c3a36a905c67ecf77b4
JustineBiancaMStaRomana <justinebianca.staromana@smc.pshs.edu.ph>
2025-01-27 10:10:54 +0800

    Changed units for problem 1

--- q3fa1p1.js
+++ q3fa1p1.js
@@ -1,6 +1,6 @@
 var nickname = "Justine";
 var height = "a\'b\"";
-var weight = "idk.idk lbs";
+var weight = "idk.idk kg";
 
 function prob1() {
     alert("Name: " + nickname + " \nHeight: " + height + " \nWeight: " + weight);
